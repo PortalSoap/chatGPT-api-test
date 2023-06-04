1da5b11a78742bba4bc2aaa8c4f6f974ac4248f5
Lucas M. Lacerda <82623575+PortalSoap@users.noreply.github.com>
2023-06-04 15:47:18 -0300

    Adição inicial
    O código fonte do projeto foi adicionado, junto com suas dependências.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace application_calling_test
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var apiKey = "Alguma key";
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+            var jsonContent = new
+            {
+                prompt = "Olá ChatGPT. O que você pode fazer?",
+                model = "text-davinci-003",
+                max_tokens = 1000
+            };
+
+            var responseContent = await client.PostAsync("https://api.openai.com/v1/completions",
+                new StringContent(JsonConvert.SerializeObject(jsonContent), Encoding.UTF8,
+                "application/json"));
+
+            var resContext = await responseContent.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<dynamic>(resContext);
+            string response = data.choices[0].text;
+
+            Console.WriteLine(response);
+        }
+    }
+}
